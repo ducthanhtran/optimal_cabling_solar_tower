7e6c05c5036ec2326e25e845bac18ba66d5784aa
duc <ducthanhtran@users.noreply.github.com>
2018-06-27 19:55:48 +0200

    added partitioning computation and Solution tuple

---
+++ src/common.py
@@ -0,0 +1,25 @@
+from typing import Dict, NamedTuple
+
+
+Edge = NamedTuple('Edge', [('v', int), ('w', int)]) # edge contains two indices to coordinates-array
+Solution = NamedTuple('Solution', [('Edges', List[Edge]), ('Degrees': Dict[int,int]), ('EdgeTypes', Dict[Edge,int]), ('VertexCosts', Dict[int,float])])
+
+def cost(sol: Solution, edge_costs: numpy.ndarray, vertex_costs: numpy.ndarray = None):
+
+
+
+def compute_partitions(coordinates: np.ndarray, partitions: int) -> List[np.ndarray]:
+    """
+    Partitions the coordinates into an array of indices. For this matter we use the angles
+    between the reference vector of the x-axis.
+
+    :param coordinates: a 2D real array of x- and y-coordinates
+    :param partitions: number of partitions
+    :return: list of arrays that contain indices of heliostats with regards to the coordinates array
+    """
+    degrees = np.degrees(np.arctan2(coordinates[1:,1], self.coordinates.iloc[1:,0]))
+    indices = degrees.argsort()
+    padding = (-len(indices))%partitions
+    L = np.split(np.concatenate((indices,np.ones(padding)*-1)),partitions) # padding value: -1
+    # remove padding from last partition and increase index
+    return [np.delete(a, np.where(a == -1)) + 1 for a in L]
