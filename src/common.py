08f3132d906bb99d8a91f64b7719b81cbc1d5763
duc <ducthanhtran@users.noreply.github.com>
2018-06-27 23:28:43 +0200

    added solution classes

--- src/common.py
+++ src/common.py
@@ -2,10 +2,46 @@ from typing import Dict, NamedTuple
 
 
 Edge = NamedTuple('Edge', [('v', int), ('w', int)]) # edge contains two indices to coordinates-array
-Solution = NamedTuple('Solution', [('Edges', List[Edge]), ('Degrees': Dict[int,int]), ('EdgeTypes', Dict[Edge,int]), ('VertexCosts', Dict[int,float])])
 
-def cost(sol: Solution, edge_costs: numpy.ndarray, vertex_costs: numpy.ndarray = None):
 
+class EdgeSolution:
+    def __init__(self, n: int, partitions: int) -> None:
+        self.edges= [[] for _ in range(partitions)] # type: List[List[Edge]]
+        self.edge_type_cost = {} # type: Dict[Edge,int]
+
+    def add_edge(self, edge: Edge, edge_type_cost: float, partition: int):
+        self.edges[partition].append(edge)
+        self.edge_type_cost[edge] = edge_type_cost
+
+    def cost(self, distances: np.ndarray):
+        return sum(distances[e.v,e.w]*self.edge_type_cost[e] for e in chain.from_iterable(self.edges))
+
+
+class EdgeVertexSolution(EdgeSolution):
+    def __init__(self, n: int, partitions: int):
+        super().__init__(n, partitions)
+        self.degrees = {i:0 for i in range(n)} # type: Dict[int,int]
+
+    def add_edge(self, edge: Edge, edge_type_cost: float, partition: int):
+        self.edges[partition].append(edge)
+        self.edge_type_cost[edge] = edge_type_cost
+        self.degrees[edge.v] += 1
+        self.degrees[edge.w] += 1
+
+    def cost(self, distances: np.ndarray):
+        cost = super().cost(distances)
+        return cost + sum(self._switch_cost(d) for d in self.degrees.values())
+
+    def _switch_cost(self, degree: int) -> float:
+        """Note: Hard-coded values for quick and dirty development"""
+        if degree <= 2:
+            return 100
+        elif degree >= 3 and degree <= 9:
+            return 800
+        elif degree >= 10 and degree <= 17:
+            return 1500
+        else:
+            raise ValueError('Degree is too high. No cost found.')
 
 
 def compute_partitions(coordinates: np.ndarray, partitions: int) -> List[np.ndarray]:
