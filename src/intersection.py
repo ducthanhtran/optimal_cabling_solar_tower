2c47ee2a4627a925982a00dabee51534160915e7
duc <ducthanhtran@users.noreply.github.com>
2018-08-10 16:00:27 +0200

    variable renaming from M to matrix. Moreover, adding spacings and annotations. The main intersection function is moved to the top

--- src/intersection.py
+++ src/intersection.py
@@ -5,16 +5,75 @@ from typing import List
 import numpy as np
 
 
-def equal_coords(M, start_l, end_l, start_r, end_r):
-    return np.all(M[:, start_l:end_l] == M[:,start_r:end_r], axis=1)
+def is_edge_intersecting(edge: np.ndarray, other_edges: List[np.ndarray]) -> bool:
+    """
+    Checks whether an edge intersects with a set of other_edges.
+
+    :param edge: array of size (1,4)
+    :param other_edges: array of shape (N,4) denoting N other_edges
+    """
+
+    other_edges = np.vstack(other_edges)
+
+    # NOTE: ugly hack
+    if len(other_edges) == 0:
+        return False
+
+    matrix = np.concatenate((np.repeat(edge, len(other_edges), axis=0), other_edges), axis=1)
+
+    # obtain masking of incident other_edges
+    incident_mask = np.logical_or.reduce((equal_coords(matrix, 0, 2, 4, 6),
+                                          equal_coords(matrix, 0, 2, 6, 8),
+                                          equal_coords(matrix, 2, 4, 4, 6),
+                                          equal_coords(matrix, 2, 4, 6, 8)))
+    matrix = matrix[np.logical_not(incident_mask)]
+    if matrix.size == 0:
+        return False
+
+    o1 = orientation(matrix, 0, 1, 2, 3, 4, 5)
+    o2 = orientation(matrix, 0, 1, 2, 3, 6, 7)
+    o3 = orientation(matrix, 4, 5, 6, 7, 0, 1)
+    o4 = orientation(matrix, 4, 5, 6, 7, 2, 3)
+
+    if np.logical_and(o1 != o2, o3 != o4).any():
+        return True
+
+    if np.logical_and(o1 == 0, on_segment(matrix, 0, 1, 4, 5, 2, 3)).any():
+        return True
+    if np.logical_and(o2 == 0, on_segment(matrix, 0, 1, 6, 7, 2, 3)).any():
+        return True
+    if np.logical_and(o3 == 0, on_segment(matrix, 4, 5, 0, 1, 6, 7)).any():
+        return True
+    if np.logical_and(o4 == 0, on_segment(matrix, 4, 5, 2, 3, 6,7 )).any():
+        return True
+    return False
 
 
-def disjoint_region(M, i, j, k, l):
-    return np.all(np.maximum(M[:,i], M[:,j]) < np.minimum(M[:,k], M[:,l]))
+def equal_coords(matrix: np.ndarray,
+                 start_l: int,
+                 end_l: int,
+                 start_r: int,
+                 end_r: int) -> np.ndarray:
+    return np.all(matrix[:, start_l:end_l] == matrix[:, start_r:end_r], axis=1)
 
 
-def orientation(M, px, py, qx, qy, rx, ry) -> np.ndarray:
-    val = (M[:,qy]-M[:,py]) * (M[:,rx]-M[:,qx]) - (M[:,qx]-M[:,px]) * (M[:,ry]-M[:,qy])
+def disjoint_region(matrix: np.ndarray,
+                    i: int,
+                    j: int,
+                    k: int,
+                    l: int) -> bool:
+    return np.all(np.maximum(matrix[:, i], matrix[:, j]) < np.minimum(matrix[:, k], matrix[:, l]))
+
+
+def orientation(matrix: np.ndaray,
+                px: int,
+                py: int,
+                qx: int,
+                qy: int,
+                rx: int,
+                ry: int) -> np.ndarray:
+    val = (matrix[:, qy] - matrix[:, py]) * (matrix[:, rx] - matrix[:, qx]) - \
+          (matrix[:, qx] - matrix[:, px]) * (matrix[:, ry] - matrix[:, qy])
     np.place(val, val > 0, 1)
     np.place(val, val < 0, 2)
     return val
@@ -25,48 +84,3 @@ def on_segment(M, px, py, qx, qy, rx, ry) -> np.ndarray:
                                   M[:,qx] >= np.minimum(M[:,px], M[:,rx]),
                                   M[:,qy] <= np.maximum(M[:,py], M[:,ry]),
                                   M[:,qy] >= np.minimum(M[:,py], M[:,ry])))
-
-
-def is_edge_intersecting(edge: np.ndarray, edge_set: List[np.ndarray], partition_indices: List[bool] = []) -> bool:
-    """
-    :param edge: array of size (1,4)
-    :param edge_set: array of shape (N,4)
-    :param partition_indices: boolean list that selects subset of edge_set
-    """
-    if not partition_indices:
-        edge_set = np.vstack(edge_set)
-    else:
-        edge_set = np.vstack(compress(edge_set, partition_indices))
-
-    # NOTE: ugly hack
-    if len(edge_set) == 0:
-        return False
-
-    M = np.concatenate((np.repeat(edge, len(edge_set), axis=0), edge_set), axis=1)
-
-    # obtain masking of incident edges
-    incident_mask = np.logical_or.reduce((equal_coords(M,0,2,4,6),
-                                          equal_coords(M,0,2,6,8),
-                                          equal_coords(M,2,4,4,6),
-                                          equal_coords(M,2,4,6,8)))
-    M = M[np.logical_not(incident_mask)]
-    if M.size == 0:
-        return False
-
-    o1 = orientation(M,0,1,2,3,4,5)
-    o2 = orientation(M,0,1,2,3,6,7)
-    o3 = orientation(M,4,5,6,7,0,1)
-    o4 = orientation(M,4,5,6,7,2,3)
-
-    if np.logical_and(o1 != o2, o3 != o4).any():
-        return True
-
-    if np.logical_and(o1 == 0, on_segment(M,0,1,4,5,2,3)).any():
-        return True
-    if np.logical_and(o2 == 0, on_segment(M,0,1,6,7,2,3)).any():
-        return True
-    if np.logical_and(o3 == 0, on_segment(M,4,5,0,1,6,7)).any():
-        return True
-    if np.logical_and(o4 == 0, on_segment(M,4,5,2,3,6,7)).any():
-        return True
-    return False
